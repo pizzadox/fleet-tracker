668673c876a32cad23652eb4b0c3bed2082c60da
Z User <z@container>
2026-05-15 10:44:45 +0000

    fb104865-be50-44c8-88ee-d030234aa3cb

--- keep-alive.sh
+++ keep-alive.sh
@@ -2,8 +2,15 @@
 cd /home/z/my-project
 export PORT=3000
 export HOSTNAME=0.0.0.0
+
+# Rotate logs if too big
+if [ -f /home/z/my-project/server.log ] && [ $(wc -c < /home/z/my-project/server.log 2>/dev/null) -gt 1048576 ]; then
+  tail -100 /home/z/my-project/server.log > /home/z/my-project/server.log.tmp
+  mv /home/z/my-project/server.log.tmp /home/z/my-project/server.log
+fi
+
 while true; do
   node .next/standalone/server.js 2>&1 >> /home/z/my-project/server.log
-  echo "[$(date)] Server crashed, restarting..." >> /home/z/my-project/server.log
-  sleep 2
+  echo "[$(date)] Server exited (code $?), restarting in 3s..." >> /home/z/my-project/server.log
+  sleep 3
 done
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Persistent production server runner
 cd /home/z/my-project
 export PORT=3000
 export HOSTNAME=0.0.0.0
