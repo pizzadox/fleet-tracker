3ad30a6355c35de620a009300bbffaed58137cee
FleetTracker <fleet-tracker@users.noreply.github.com>
2026-05-16 07:07:20 +0000

    Подготовка к публикации: обновлён .gitignore, добавлен README.md, убраны служебные файлы

--- run-dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-export PORT=3000
-export HOSTNAME=0.0.0.0
-exec npx next dev -p 3000 -H 0.0.0.0 2>&1
--- start-prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-export PORT=3000
-export HOSTNAME=0.0.0.0
-exec node .next/standalone/server.js 2>&1
